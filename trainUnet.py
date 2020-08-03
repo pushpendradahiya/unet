6c7d522e9ec5987a1375aa51999e8f5ba337f9f9
pushpendradahiya <pushpendradahiya@users.noreply.github.com>
2020-08-03 16:04:55 +0530

    Unet Code Updated and changed
    Unet and Alexnet for for brain tumor detection/ classification

--- trainUnet.py
+++ trainUnet.py
@@ -1,16 +1,21 @@
-import numpy as np 
-import os
-import skimage.io as io
-import skimage.transform as trans
-import numpy as np
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
 from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, ReduceLROnPlateau, Callback
 from keras import backend as keras
+from sklearn.utils import class_weight
+import h5py
 
 
-def unet(pretrained_weights = None,input_size = (240,240,1)):
+# In[7]:
+
+
+def unet(pretrained_weights = None,input_size = (96,96,1)):
     inputs = Input(input_size)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
@@ -63,4 +68,82 @@ def unet(pretrained_weights = None,input_size = (240,240,1)):
 
     return model
 
+
+# In[8]:
+
+
+import SimpleITK as sitk
+import os
+import numpy as np
+#import matplotlib.pyplot as plt
+
+
+# In[9]:
+
+
+def find_images(path_dir):
+    X_train=np.zeros((274*60,96,96),dtype=np.uint16)
+    Y_train=np.zeros((274*60,96,96),dtype=np.uint16)
+    j=0
+    for item in os.listdir(path_dir):
+        item=os.path.join(path_dir,item)
+        for item2 in os.listdir(item):
+            im ={'T1':None,'gt':None}
+            item2=os.path.join(item,item2)
+            for item3 in os.listdir(item2):
+                item3=os.path.join(item2,item3)
+                for item4 in os.listdir(item3):
+                    item5=os.path.join(item3,item4)
+                    if os.path.isfile(item5) and item5.endswith('.mha'):
+                        itk_image = sitk.ReadImage(item5)
+                        nd_image = sitk.GetArrayFromImage(itk_image)
+                        if 'more' in item5 or 'OT' in item5:
+                            im['gt']=nd_image
+                        elif 'T1' in item5 and 'T1c' not in item5:
+                            im['T1']=nd_image
+            for i in range(60,120):
+                Y_train[j]=np.where(im['gt'][i,24:216:2,24:216:2] > 0, 1, 0)
+                X_train[j]=im['T1'][i,24:216:2,24:216:2]
+                j+=1
+    return X_train,Y_train
+
+
+# In[10]:
+
+
+path_dir="../../BRATS2015_Training"
+X_train,Y_train=find_images(path_dir)
+X_train=X_train/np.amax(X_train)
+#Y_train=Y_train-np.mean(Y_train,axis=0)
+shape=X_train.shape
+X_train=X_train.reshape(shape[0],shape[1],shape[2],1)
+Y_train=Y_train.reshape(shape[0],shape[1],shape[2],1)
+with h5py.File('x_train.h5', 'w') as hf:
+    hf.create_dataset("x_train",  data=X_train)
+with h5py.File('y_train.h5', 'w') as hf:
+    hf.create_dataset("y_train",  data=Y_train)
+
+
+# In[11]:
+
+
+model = unet(input_size = (96,96,1))
+model_checkpoint = ModelCheckpoint('unet_brat2.hdf5', monitor='val_loss',verbose=1, save_best_only=True)
+reduce_lr = ReduceLROnPlateau(factor=0.5, patience=5, min_lr=0.000001, verbose=1)
+callbacks = [reduce_lr, model_checkpoint]
+#class_weights = class_weight.compute_class_weight('balanced', np.unique(Y_train.ravel()), Y_train.ravel())
+class_weights = [1,3]
+model.load_weights("./unet_brat2.hdf5")
+model.fit(X_train, Y_train, 
+    batch_size=32, 
+    nb_epoch=200, 
+    verbose=1,
+    validation_split=0.2, 
+    shuffle=True,
+    class_weight=class_weights,
+    callbacks= callbacks)
+
+
+# In[3]:
+model.save('unet_train.hdf5')
 
